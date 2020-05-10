ab49d75c5d6753a9cbcf68d61521cba5b76dc253
Kyle Booth <kbooth1000@gmail.com>
2020-05-09 23:49:19 -0400

    Adds instagram component

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,9 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+const config = require('./data/config');
+
+
 module.exports = {
   pathPrefix: `/darkhorse`,
   siteMetadata: {
@@ -15,6 +18,46 @@ module.exports = {
     author: 'Kyle J Booth'
   },
   plugins: [
+
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/static/images`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-sharp`,
+    },
+    {
+      resolve: `gatsby-plugin-sharp`,
+    },
+    `gatsby-plugin-sharp`,
+
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-instagram`,
+      options: {
+        // type: `user-profile`,
+        username: config.social.instagram,
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        alias: {
+          components: `${__dirname}/src/components`,
+          common: `${__dirname}/src/components/common/`,
+          static: `${__dirname}/static/`,
+          theme: `${__dirname}/src/components/theme`,
+          data: `${__dirname}/data/`,
+        },
+      },
+    },
+
+
     {
       resolve: 'gatsby-source-graphql',
       options: {
---
+++ src/components/imageGallery.js
@@ -0,0 +1,72 @@
+import { styled } from 'baseui';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
+import * as React from 'react';
+import '../styles/instaStyles.css';
+
+const Image = styled(Img, {
+  objectFit: 'cover',
+  objectPosition: '100% 0',
+  width: '55px',
+  height: '55px',
+  // maxHeight: '25rem',
+});
+
+
+const nodeURL = 'https://www.instagram.com/p';
+
+const ImageGallery = () => {
+  const {
+    allInstaNode: { edges },
+  } = useStaticQuery(graphql`
+    {
+      allInstaNode(sort: { fields: timestamp, order: DESC }, limit: 12) {
+        edges {
+          node {
+            id
+            caption
+            localFile {
+              childImageSharp {
+                fluid {
+                  ... GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+    const images = edges.map(({ node }) => {
+      const {
+        id,
+        caption,
+        localFile: { childImageSharp },
+      } = node;
+      return (
+
+          <a href={`${nodeURL}/${id}`}>
+            <Image
+              loading="lazy"
+              alt={caption || ''}
+              fluid={childImageSharp.fluid}
+            />
+          </a>
+
+      );
+    });
+
+
+
+  return (
+
+
+    <div className="instaBox">
+      {images}
+      
+    </div>
+  );
+};
+
+export default ImageGallery;
