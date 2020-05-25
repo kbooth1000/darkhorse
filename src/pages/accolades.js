081358d7fc093d5a22244cf9ce77d7dd084c58b4
Kyle Booth <kbooth1000@gmail.com>
2020-05-25 13:01:31 -0400

    Adds Reviews page; fixes footer

---
+++ src/pages/accolades.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/Layout';
+
+import Head from '../components/Head';
+
+const Accolades = () => (<div className="Accolades">
+
+  <Layout title="Accolades">
+
+    <Head />
+    <div class="entry-content" itemprop="text">
+      <div class="bne-testimonial-wrapper testimonial-wrapper-id-284 testimonial-layout-list testimonial-theme-default testimonial-left testimonial-arrangement-1 testimonial-category-all accolade fancybox clearfix">
+        <div id="testimonial-id-1213" class="testimonial-single clearfix">
+          <div class="testimonial-author">
+            <img width="150" height="150" src="https://i2.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2016/02/066-e1503672287973.jpg?resize=150%2C150" class="testimonial-image testimonial-square testimonial-crop-thumbnail wp-post-image jetpack-lazy-image jetpack-lazy-image--handled" alt="Ann B." srcset="https://i2.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2016/02/066-e1503672287973.jpg?zoom=2&amp;resize=150%2C150 300w, https://i2.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2016/02/066-e1503672287973.jpg?zoom=3&amp;resize=150%2C150 450w" data-lazy-loaded="1" sizes="(max-width: 150px) 100vw, 150px" />
+            <span class="testimonial-name">Ann B.</span>
+            <span class="testimonial-website">
+              <Link to="/project/balmoral-2/" target="_blank" title="Balmoral" rel="nofollow">Balmoral</Link>
+            </span>
+          </div>
+          <div class="testimonial-content">
+            <div class="testimonial-message">
+              <p>Dark Horse Woodworks did a wonderful job on our kitchen and master bath. Through the years, past owners had brought in traditional elements into our mid-century modern home, but now,  thanks to Chris, it’s back to mid-century. Working with our designers, Chris designed and built beautiful mid-century cabinetry. The project took about five months from design through completion, and Chris was easy to work with at all phases of the project.<br /> I originally found Dark Horse Woodworks through Houzz when I was looking for inspiration for our new kitchen. I was so excited when I noticed that my favorite kitchen had been built in Atlanta by Dark Horse. Now things have come full circle, and I’ve <a href="http://www.houzz.com/viewReview/550122/Dark-Horse-Woodworks-review" target="_blank">posted a review on Houzz</a>! Please consider Dark Horse Woodworks for your project – Chris is a real perfectionist and you'll love the finished project!</p>
+            </div>
+          </div>
+          <div class="clear">
+          </div>
+        </div>
+        <div id="testimonial-id-1214" class="testimonial-single clearfix">
+          <div class="testimonial-author">
+            <img width="150" height="150" src="https://i2.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2016/08/11079541_10153309355357044_7198761877723531567_o.jpg?resize=150%2C150" class="testimonial-image testimonial-square testimonial-crop-thumbnail wp-post-image jetpack-lazy-image jetpack-lazy-image--handled" alt="Joanne K." srcset="https://i2.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2016/08/11079541_10153309355357044_7198761877723531567_o.jpg?resize=150%2C150 150w, https://i2.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2016/08/11079541_10153309355357044_7198761877723531567_o.jpg?resize=300%2C300 300w, https://i2.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2016/08/11079541_10153309355357044_7198761877723531567_o.jpg?zoom=3&amp;resize=150%2C150 450w" data-lazy-loaded="1" sizes="(max-width: 150px) 100vw, 150px" />
+            <span class="testimonial-name">Joanne K.</span>
+            <span class="testimonial-website">
+              <Link to="/project/the-highland-house/" target="_blank" title="Highland House" rel="nofollow">Highland House</Link>
+            </span>
+          </div>
+          <div class="testimonial-content">
+            <div class="testimonial-message">
+              <p>Chris Dehmer and his team built the amazing walnut cabinetry in our kitchen and mud room, along with floating shelving in our home office and master bed and bath.<br /> Their craftsmanship is beyond perfection! The attention to detail has won this amazing team well-deserved national recognition.</p>
+            </div>
+          </div>
+          <div class="clear">
+          </div>
+        </div>
+        <div id="testimonial-id-187" class="testimonial-single clearfix">
+          <div class="testimonial-author">
+            <img width="150" height="150" src="https://i1.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2014/11/kitchen-fireplace.jpg.jpeg?resize=150%2C150" class="testimonial-image testimonial-square testimonial-crop-thumbnail wp-post-image jetpack-lazy-image jetpack-lazy-image--handled" alt="Donna B." srcset="https://i1.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2014/11/kitchen-fireplace.jpg.jpeg?resize=150%2C150 150w, https://i1.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2014/11/kitchen-fireplace.jpg.jpeg?resize=300%2C300 300w, https://i1.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2014/11/kitchen-fireplace.jpg.jpeg?zoom=3&amp;resize=150%2C150 450w" data-lazy-loaded="1" sizes="(max-width: 150px) 100vw, 150px" />
+            <span class="testimonial-name">Donna B.</span>
+            <span class="testimonial-website">
+              <Link to="/project/lanbrook-kitchen/" target="_blank" title="Lanbrook" rel="nofollow">Lanbrook</Link>
+            </span>
+          </div>
+          <div class="testimonial-content">
+            <div class="testimonial-message">
+              <p>Chris and the boys from Dark Horse Woodworks knocked this out of the park! We came to Chris earlier this year with a dream, and he made it a reality. Yes, we had a picture as our starting point, but as we all know, that only gets you about 10 percent there. Chris and his team then had to figure out how to do that in our space. They combated crooked walls, an uneven ceiling and sloping floors –not to mention also dealing with me changing my mind a few times throughout the project!</p>
+              <p>Chris’s attention to detail is borderline obsessive, but we appreciated it. Not only does the cabinetry look beautiful, but it also functions beautifully – the key to a good working kitchen. Chris wasn’t satisfied until everything was perfect, and it paid off. We couldn’t be happier with the result. I’d highly recommend Chris and the Dark Horse team.<a name="02">
+              </a>
+              </p>
+            </div>
+          </div>
+          <div class="clear">
+          </div>
+        </div>
+        <div id="testimonial-id-186" class="testimonial-single clearfix">
+          <div class="testimonial-author">
+            <img width="150" height="150" src="https://i0.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2014/11/IMG_0257-e1416847586402.jpg?resize=150%2C150" class="testimonial-image testimonial-square testimonial-crop-thumbnail wp-post-image jetpack-lazy-image jetpack-lazy-image--handled" alt="Bret W." srcset="https://i0.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2014/11/IMG_0257-e1416847586402.jpg?zoom=2&amp;resize=150%2C150 300w, https://i0.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2014/11/IMG_0257-e1416847586402.jpg?zoom=3&amp;resize=150%2C150 450w" data-lazy-loaded="1" sizes="(max-width: 150px) 100vw, 150px" />
+            <span class="testimonial-name">Bret W.</span>
+            <span class="testimonial-website">
+              <Link to="/project/oakview-home/" target="_blank" title="Oakview" rel="nofollow">Oakview</Link>
+            </span>
+          </div>
+          <div class="testimonial-content">
+            <div class="testimonial-message">
+              <p>We loved working with Dark Horse. Not only are they great craftsmen with a confidence-inspiring attention to detail, but Chris has a unique understanding of wood. He suggested and sourced several unusual varieties that made a significant difference in our house. Everyone who comes in comments on the warm, beautiful wood. Thanks Chris!<a name="01">
+              </a>
+              </p>
+            </div>
+          </div>
+          <div class="clear">
+          </div>
+        </div>
+        <div id="testimonial-id-168" class="testimonial-single clearfix">
+          <div class="testimonial-author">
+            <img width="150" height="150" src="https://i1.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2014/11/ss-kitchen.jpg?resize=150%2C150" class="testimonial-image testimonial-square testimonial-crop-thumbnail wp-post-image jetpack-lazy-image jetpack-lazy-image--handled" alt="Julian A." srcset="https://i1.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2014/11/ss-kitchen.jpg?resize=150%2C150 150w, https://i1.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2014/11/ss-kitchen.jpg?resize=300%2C300 300w, https://i1.wp.com/darkhorsewoodworks.com/dh1/wp-content/uploads/2014/11/ss-kitchen.jpg?zoom=3&amp;resize=150%2C150 450w" data-lazy-loaded="1" sizes="(max-width: 150px) 100vw, 150px" />
+            <span class="testimonial-name">Julian A.</span>
+            <span class="testimonial-website">
+              <Link to="/project/brandon-mill-home/" target="_blank" title="Brandon Mill" rel="nofollow">Brandon Mill</Link>
+            </span>
+          </div>
+          <div class="testimonial-content">
+            <div class="testimonial-message">
+              <p>We couldn't be happier with the work that Chris and his team at Dark Horse did in our home. What doesn't show up in these great photos is the amazing level of detail they used to get everything just right. Half of what you see in our kitchen is original, built in 1964. Chris and his team updated the original cabinets and built new ones to match them perfectly. We were so impressed with the quality of their work in the kitchen that we commissioned the custom audio/video wall. My partner, Dallas, designed it, and then worked with Chris to make sure that every detail was just right. (Who knew you could have so many conversations about the direction of wood grains?!)</p>
+            </div>
+          </div>
+          <div class="clear">
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </Layout>
+
+</div>
+)
+
+export default Accolades;
